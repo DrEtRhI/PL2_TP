438475dd08f04e5e25bf6a5c76560f78854b373e
DrEtRhI <thierryeric@free.fr>
2016-11-29 17:49:48 +0100

    TD1

---
+++ TD1/HelloWorld/HelloWorldApp.java
@@ -0,0 +1,11 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+	public static void main(String[] args){
+		System.out.println("Bonjour");
+		System.out.println("Ceci est mon"); //Display the string.
+		System.out.println("Programme sous Java");
+	}
+}
