c9a810ad7cfcc556450ae7ebd7c3576e4d586942
DrEtRhI <thierryeric@free.fr>
2016-12-15 16:58:35 +0100

    TD8

---
+++ TD8/AnimationChenille/src/animationchenille/Anneau.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animationchenille;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author thierrye
+ */
+public class Anneau {
+
+    protected int x;
+    protected int y;
+    protected int r = 5;
+
+    /**
+     * crée un Anneau en fixant sa position initiale et son rayon
+     *
+     * @param xInit abscisse du centre de l'anneau
+     * @param yInit ordonnée du centre de l'anneau
+     * @param r rayon de l'anneau
+     */
+    public Anneau(int xInit, int yInit, int r) {
+        this.x = xInit;
+        this.y = yInit;
+        this.r = r;
+    }
+
+    /**
+     * crée un Anneau en fixant sa position initiale, son rayon est de 5 par
+     * défaut
+     *
+     * @param xInit abscisse du centre de l'anneau
+     * @param yInit ordonnée du centre de l'anneau
+     */
+    public Anneau(int xInit, int yInit) {
+        this.x = xInit;
+        this.y = yInit;
+    }
+
+    /**
+     * retourne abscisse du centre de l'anneau
+     *
+     * @return abscisse du centre de l'anneau
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * retourne ordonnée du centre de l'anneau
+     *
+     * @return ordonnée du centre de l'anneau
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * positionne le centre de l'anneau en un point donné
+     *
+     * @param px abscisse du point
+     * @param py ordonnée du point
+     */
+    void placerA(int px, int py) {
+
+    }
+
+    /**
+     * affiche l'anneau en le matérialisant par un cercle noir
+     *
+     * @param g objet de classe java.awt.Graphics qui prend en charge la gestion
+     * de l'affichage dans la fenêtre de dessin
+     */
+    void dessiner(Graphics g) {
+        g.drawOval(x - r, y - r, r * 2, r * 2);
+    }
+
+}
---
+++ TD8/AnimationChenille/src/animationchenille/Dessin.java
@@ -0,0 +1,88 @@
+package animationchenille;
+
+/**
+ * Dessin.java
+ */
+import java.awt.Graphics;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+/**
+ * Defini le contenu de la fenêtre de l'application d'animation des Chenille.
+ * Une zone de dessin est un JPanel qui gère un liste d'objets Chenille.
+ * Lorsqu'il se réaffiche l'objet Dessin redessinne les différents objets
+ * Chenille contenus dans cette liste.
+ *
+ * @author Philippe Genoud
+ * @version
+ */
+public class Dessin extends JPanel {
+
+    /**
+     * stocke la liste des Chenille ayant été ajoutées à cette zone de dessin.
+     */
+    private final List<Chenille> listeDesChenilles = new ArrayList<>();
+
+    /**
+     * retourne la largeur de la zone de dessin.
+     *
+     * @return la largeur.
+     */
+    public int getLargeur() {
+        return getWidth();
+    }
+
+    /**
+     * retourne la hauteur de la zone de dessin.
+     *
+     * @return la hauteur.
+     */
+    public int getHauteur() {
+        return getHeight();
+    }
+
+    /**
+     * ajoute un Chenille à la zone de dessin.
+     *
+     * @param c la Chenille à ajouter au Dessin
+     * @see Chenille
+     */
+    public void ajouterObjet(Chenille c) {
+
+        if (!listeDesChenilles.contains(c)) {
+            // l'objet n'est pas déjà dans la liste
+            // on le rajoute a la liste des objets du dessin
+            listeDesChenilles.add(c);
+            // le dessin se réaffiche
+            repaint();
+            this.pause(10);
+        }
+    }
+
+    /**
+     * temporisation de l'animation.
+     * @param duree delai de temporisation en ms.
+     */
+    public void pause(int duree) {
+        try {
+            Thread.sleep(duree);
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * affiche la zone de dessin et son contenu
+     *
+     * @param g le contexte graphique
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // on dessine chacun des visages contenus dans la zone de dessin
+        for (Chenille v : listeDesChenilles) {
+            v.dessiner(g);
+        }
+    }
+
+} // Dessin
